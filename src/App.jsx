47e0b49ccd1ee40ca14d20d9a57fcea4dc1a8392
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-07-31 23:42:03 -0300

    Criação e estilização do card ReactJS

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ function App() {
 
   return (
     <>
-     
+     <h1>Este é um título h1 em ReactJS</h1>
+     <div className='card'>
+      <h2>Lutadores Street Fight</h2>
+      <img src='https://criticalhits.com.br/wp-content/uploads/2018/02/blanka.jpg' width='300' />
+
+     </div>
     </>
   )
 }
